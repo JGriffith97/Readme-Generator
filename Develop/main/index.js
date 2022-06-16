c107c81278639a7f0bfab56945c632e44a52697b
Jayden Griffith <griffithjayden97@outlook.com>
2022-06-15 18:39:16 -0600

    Added value to be passed into inquirer.

--- Develop/main/index.js
+++ Develop/main/index.js
@@ -3,13 +3,52 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'linkedIn',
+    message: 'What is your LinkedIn URL?',
+  },  
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Append? or Write file?
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
+
+//----------------------------------------------------------------------------------
+
+
+module.exports = {
+  questions
+}
+
+
+
+// For future reference, just so I can hot-copy/paste
+// https://www.linkedin.com/in/jayden-griffith-a3b7b9217/
+// https://github.com/JGriffith97
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,5 @@
+const questions = require('../main/index.js')
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
