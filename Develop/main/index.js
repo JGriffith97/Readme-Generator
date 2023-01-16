5d2ce6161baaf867a929abe8a452a6f3db7921b2
Jayden Griffith <griffithjayden97@outlook.com>
2023-01-16 10:14:10 -0700

    Removed todos, and extraneous comments from index.js

--- Develop/main/index.js
+++ Develop/main/index.js
@@ -1,6 +1,3 @@
-// TODO: Include packages needed for this application
-// const fs = require('fs');
-// const inquirer = require('inquirer')
 const generateMd = require('./generateMarkdown')
 
 function initialize() {
